4676097906e002373b5aae8490bcd00637422345
Sachio <sachiokawasaki@gmail.com>
2026-05-21 14:33:00 +0100

    Set authDomain to auth.vual.jp

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,7 +4,7 @@ import { getFirestore, Firestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY?.trim(),
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN?.trim(),
+  authDomain: 'auth.vual.jp',
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID?.trim(),
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET?.trim(),
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID?.trim(),
