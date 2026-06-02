6d08d72c23b5cc7fdbeda419682aa01998324192
Sachio <sachiokawasaki@gmail.com>
2026-06-02 10:47:46 +0100

    Implant modal: add bottom padding

--- src/components/ImplantModal.tsx
+++ src/components/ImplantModal.tsx
@@ -269,7 +269,7 @@ export function ImplantModal({ image, entities, themeCity, totalLooks, onClose }
   return (
     <>
       <div
-        className="fixed inset-0 z-50 flex items-end justify-center"
+        className="fixed inset-0 z-50 flex items-end justify-center pb-6"
         onClick={handleClose}
       >
         {/* Backdrop */}
